7573a74770a4b2ff74f96065a82a139b278e2584
obai Albek <3009594@stud.hs-mannheim.de>
2025-09-06 20:34:21 +0200

    init Project

---
+++ spreadsheet/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module spreadsheet {
+}
---
+++ spreadsheet/src/spreadsheet/app/App.java
@@ -0,0 +1,7 @@
+package spreadsheet.app;
+
+public class App {
+	public static void main(String... args) {
+		System.out.println("Hello World!");
+	}
+}
